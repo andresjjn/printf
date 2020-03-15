a86b19c0707f8af8355585cb194a08c32dfea3db
davehh1211 <davehh1211@gmail.com>
2020-03-15 15:57:54 +0000

    add library and functions file 1

---
+++ holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+int _printf(const char *format, ...);
+int _putchar(char c);
+#endif
