e930c9df6b318d2b8c98866f1ec123900c5c9572
andresjjn <1453@holbertonschool.com>
2020-03-16 21:53:57 +0000

    Fix: New prototypes in holberton.c and remove no necesary vars in _printf.c

--- holberton.h
+++ holberton.h
@@ -7,4 +7,7 @@
 #include <unistd.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
+void print_number(int n);
+void porcent(const char *format, int h, va_list arg);
+void back(const char *format, int h);
 #endif
